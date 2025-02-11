62e73a3bfb1fe34f2b366a640596d31c825f5dd8
Shing Lyu <3250983+shinglyu@users.noreply.github.com>
2025-02-11 11:15:00 +0100

    Make transcribe record by itself

--- transcribe.py
+++ transcribe.py
@@ -6,7 +6,14 @@
 # ///
 import time
 import subprocess
+import os
 from faster_whisper import WhisperModel
+
+RECORDING_PATH = "/tmp/recordings"
+RECORDING_FILE = os.path.join(RECORDING_PATH, "recording.wav")
+
+# Ensure the recording directory exists
+os.makedirs(RECORDING_PATH, exist_ok=True)
 
 # model_size = "large-v3"
 model_size = "small.en"
@@ -18,25 +25,45 @@ model_size = "small.en"
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
-print(f"Transcribing...")
 start_time = time.time()
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-segments, info = model.transcribe("/tmp/recordings/recording.wav", beam_size=1, language="en")
-
-# print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-
-# Copy the segment texts to the clipboard
-transcription = " ".join([segment.text for segment in segments])
-transcription = transcription.strip()
 end_time = time.time()
+print(f"Model loading time: {end_time - start_time:.2f} seconds")
 
+while True:
+    print("Recording... Press Ctrl+C to stop.")
+    try:
+        subprocess.run(
+            ["sox", "-d", "-r", "16000", "-c", "1", "-b", "16", RECORDING_FILE],
+            check=True,
+            stderr=subprocess.DEVNULL, # Suppress "sox WARN" messages
+        )
+    except subprocess.CalledProcessError:
+        print("Recording stopped.")
+    except KeyboardInterrupt:
+        print("Recording stopped.")
 
-print(f"Model: {model_size}")
-print(f"Transcription time: {end_time - start_time:.2f} seconds")
-print(f"Transcription: {transcription}")
+    print(f"Transcribing...")
+    start_time = time.time()
 
-subprocess.run(["wl-copy", transcription])
-print(f"Transcription copied to clipboard")
+    segments, info = model.transcribe(RECORDING_FILE, beam_size=1, language="en")
 
+    # print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
+    # Copy the segment texts to the clipboard
+    transcription = " ".join([segment.text for segment in segments])
+    transcription = transcription.strip()
+    end_time = time.time()
+
+    print(f"Model: {model_size}")
+    print(f"Transcription time: {end_time - start_time:.2f} seconds")
+    print(f"Transcription copied to clipboard")
+    print("+" + "-- Transcription " + "-" * 33 + "+")
+    print(transcription)
+    print("+" + "-" * 50 + "+")
+    subprocess.run(["wl-copy", transcription])
+    print("")
+
+    # Start again
+    if input("Press Enter to start recording, or type 'q' + Enter to quit: ").lower() == 'q':
+        break
