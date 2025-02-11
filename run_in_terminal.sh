f9a688979b8fab08f708ed5ee2a0dac2e2301af6
Shing Lyu <3250983+shinglyu@users.noreply.github.com>
2025-02-11 11:19:45 +0100

    Directly run the transcribe.py

--- run_in_terminal.sh
+++ run_in_terminal.sh
@@ -2,8 +2,8 @@
 
 # Define the path to the script
 # Find the scirpt in the same folder as this script
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/record_and_transcribe.sh"
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Open a new terminal and source the script
-gnome-terminal -- bash -c "source ${SCRIPT_PATH}; sleep 5; exit"
+gnome-terminal -- bash -c "OMP_NUM_THREADS=4 uv run ${SCRIPT_PATH}/transcribe.py; sleep 5; exit"
 # In gnome-terminal Edit > Rreference > Command > When command exists: Exit the teriminal"
